15479a7bb39c2bc212a6df7d0e3f0a6245dd4823
kmacsparran <kenneth.macsparran@gmail.com>
2014-05-13 09:11:38 +0000

    Added more/better in-line documentation in label.js. Also updated the Grunt configuration.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,8 +12,21 @@ module.exports = function (grunt) {
                 files: ['std.css'],
                 tasks: ['autoprefixer']
             }
-        }
+        },
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'std.css': 'styles.scss',
+				}
+			}
+		}
     });
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	
+	grunt.registerTask('default', ['sass']);
 };
--- label.js
+++ label.js
@@ -4,15 +4,16 @@
  * Author: Kenneth MacSparran [kenneth.macsparran@gmail.com]
  * Licensed under the MIT license [http://opensource.org/licenses/MIT]
  */
+
 var LabelOptions = {
-	parentElementID: "demoForm",
-	activeClassName: "active",
-	defaultClass: "invisible",
-	toggleClass: "visible",
-	transitionInterval: 300
+	parentElementID: "activeform",
+	activeStyle: "active",
+	defaultStyle: "invisible",
+	toggleStyle: "visible"
 };
 
 var Label = function (control, context) {
+	// reference the parent html element, used to limit DOM traversal while looking for the label element
 	this.context = context;
 	/*
 	 * create reference to the current form control. 
@@ -24,15 +25,15 @@ var Label = function (control, context) {
 	this.label = this.getLabel();
 	
 	// setup for css animations
-	this.addClass(LabelOptions.activeClassName);
+	this.addClass(LabelOptions.activeStyle);
 	
 	// apply default css styles
-	this.addClass(LabelOptions.defaultClass);
+	this.addClass(LabelOptions.defaultStyle);
 	
 	// if the control already has a value assigned, 
 	if (this.control.value.length > 0) {
 		// display the control's label element.
-		this.addClass(LabelOptions.toggleClass);
+		this.addClass(LabelOptions.toggleStyle);
 	}
 	
 	// apply eventlisteners
@@ -41,68 +42,72 @@ var Label = function (control, context) {
 	return this;
 };
 Label.prototype.getLabel = function() {
-	/* 
-	 * attempt to locate the associated label element by matching 
-	 * the "htmlfor" property of label elements against the name (or id) of this control element.
-	 */
 	var labelArray = this.context.getElementsByTagName("label"),
 		labels = labelArray.length - 1,
 		i = 0;
 
 	do {
-		// since labels can reference elements by id or name, check against both
+		// labels can reference elements by id or name, so check against both
 		if (labelArray[i].htmlFor === ((this.control["name"] !== null) ? this.control["name"] : this.control.id)) {
+
+			// when a match is found, return the reference to the matching label element
 			return labelArray[i];
 		}
 	}
 	while (i++ < labels);
 };
-Label.prototype.showLabel = function() {
+Label.prototype.toggleLabel = function() {
+	// the current control has characters entered as a value
 	if (this.control.value.length > 0) {
-		this.addClass(LabelOptions.toggleClass);
+	
+		// add LabelOptions.toggleStyle to the label element, making it visible
+		this.addClass(LabelOptions.toggleStyle);
 	}
+	
+	// the current control does not have characters entered as a value
 	else {
-		this.removClass(LabelOptions.toggleClass);
+	
+		// remove LabelOptions.toggleStyle to the label element, making it invisible
+		this.removClass(LabelOptions.toggleStyle);
 	}
 };
 Label.prototype.addClass = function (name) {
 	var currentClass = (this.label.className || "").split(" "),
-		name = name || LabelOptions.activeClassName;
+		name = name || LabelOptions.activeStyle;
 	
+	// check whether the current class attribute does not include the specified string (name)
 	if (currentClass.indexOf(name) < 0) {
-		this.label.className = (currentClass.concat(name)).join(" ").trim();
+		
+		// add the specified string (name)
+		this.label.className = this.convertArrayToString(currentClass.concat(name));
 	}
 };
 Label.prototype.removClass = function (name) {
 	var currentClass = (this.label.className || "").split(" "),
-		name = name || LabelOptions.activeClassName,
+		name = name || LabelOptions.activeStyle,
 		classIndex = currentClass.indexOf(name);
 	
+	// check whether the current class attribute includes the specified string (name)
 	if (classIndex > -1) {
+		
+		// remove the specified string (name) if it is present
 		currentClass.splice(classIndex, 1);
-		this.label.className = currentClass.join(" ").trim();
+		
+		// assign the new class value
+		this.label.className = this.convertArrayToString(currentClass);
 	}
+};
+Label.prototype.convertArrayToString = function (arr) {
+	// return the array as a string, with spaces separating the array's values
+	return arr
+		// join the array elements, separated by spaces
+		.join(" ")
+		
+		// remove extra spaces
+		.trim();
 };
 Label.prototype.activate = function () {
-	this.control.addEventListener("input", this.showLabel.bind(this));
-	this.control.addEventListener("change", this.showLabel.bind(this));
+	// add an eventlistener for "oninput" and "onchange" events
+	this.control.addEventListener("input", this.toggleLabel.bind(this));
+	this.control.addEventListener("change", this.toggleLabel.bind(this));
 };
-
-(function (window, document, undefined) {
-	var userForm = document.getElementById(LabelOptions.parentElementID),
-		labels = [],
-		elemArray = userForm.getElementsByTagName("input"),
-		elems = elemArray.length - 1,
-		elem = undefined,
-		i = 0;
-
-	do {
-		elem = elemArray[i];
-		
-		if (["button", "submit", "radio", "checkbox"].indexOf(elem["type"]) < 0) {
-			labels.push(new Label(elemArray[i], userForm));
-		}
-	}
-	while (i++ < elems);
-
-})(window, document)
